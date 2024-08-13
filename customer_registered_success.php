86211397896fe8fed0f82b8fb1cb7e351d0f9dca
Prabhanjan R <109028373+iamprabhanjan@users.noreply.github.com>
2024-08-13 07:10:10 +0530

    Update customer_registered_success.php

--- customer_registered_success.php
+++ customer_registered_success.php
@@ -169,7 +169,7 @@ $conn->close();
         <hr>
         <div class="row">
             <div class="col-sm-6">
-                <h5>© 2024 Abhiram Hatwar</h5>
+                <h5>© 2024 Prabhanjan Rr</h5>
             </div>
         </div>
     </div>
